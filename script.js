4590cf4d06b09732ca3026b469c7082e4c3903bb
Isaac Yeap Sheng Rong <isaacyeapsr@gmail.com>
2022-10-12 17:43:32 +0800

    a lot of work weichu why slacking

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ window.addEventListener('click',(e)=>{
 
 const openSideBar=()=>{
     document.getElementById('sidebar').style.width = '250px'
-    document.getElementById('sidebar').style.borderLeft = '2px solid #555'
     document.getElementById('sidecover').style.width = '100%'
 }
 const closeSideBar=()=>{
     document.getElementById('sidebar').style.width = '0px'
-    document.getElementById('sidebar').style.borderLeft = ''
     document.getElementById('sidecover').style.width = '0'
 }

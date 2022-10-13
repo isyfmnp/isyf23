5ee7a54e8c3880a0ab697483c8af1bdf6f800836
Liu Weichu <weichufmsp@gmail.com>
2022-10-13 12:25:47 +0800

    navbar now closes on clicking sidecover

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const openSideBar=()=>{
-    document.getElementById('sidebar').style.width = '250px'
-    document.getElementById('sidecover').style.width = '100%'
-}
-const closeSideBar=()=>{
-    document.getElementById('sidebar').style.width = '0px'
-    document.getElementById('sidecover').style.width = '0'
-}
+const openSideBar = () => {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("sidecover").style.width = "100%";
+};
+const closeSideBar = () => {
+  document.getElementById("sidebar").style.width = "0px";
+  document.getElementById("sidecover").style.width = "0";
+};
 
 //Event Listeners
-document.getElementById('menu').addEventListener('click',openSideBar)
-document.getElementById('menuh').addEventListener('click',openSideBar)
-document.getElementById('closes').addEventListener('click',closeSideBar)
+document.getElementById("menu").addEventListener("click", openSideBar);
+document.getElementById("menuh").addEventListener("click", openSideBar);
+document.getElementById("closes").addEventListener("click", closeSideBar);
+document.getElementById("sidecover").addEventListener("click", closeSideBar);

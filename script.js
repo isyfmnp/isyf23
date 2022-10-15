558ca520cc3cd35f4203e5a0624f9207aa669662
Isaac Yeap Sheng Rong <isaacyeapsr@gmail.com>
2022-10-15 17:30:18 +0800

    minor

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ const closeSideBar = () => {
   document.getElementById("sidecover").style.width = "0";
 };
 
+//navbar
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("ham")[0].style.top = "20px";
+    document.getElementsByClassName("social")[0].style.top = "20px";
+  } else {
+    document.getElementsByClassName("ham")[0].style.top = "-200px";
+    document.getElementsByClassName("social")[0].style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 //Event Listeners
 document.getElementById("menu").addEventListener("click", openSideBar);
 document.getElementById("menuh").addEventListener("click", openSideBar);

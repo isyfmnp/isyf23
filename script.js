d86978c11103d800cae7f6bc52205dd183497bd7
Liu Weichu <weichufmsp@gmail.com>
2022-11-05 17:46:16 +0800

    update navbar add speakers page

--- script.js
+++ script.js
@@ -23,6 +23,53 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
+ismobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+let sb_collapsables = document.querySelectorAll(
+  "#sidebar .sidebar-collapsable"
+);
+sb_collapsables.forEach((sb_collapsable) => {
+  let title = sb_collapsable.children[0];
+  let content = sb_collapsable.children[1];
+
+  // mobile
+  title.addEventListener("click", function (e) {
+    if (!ismobile) return;
+    // #sidebar .sidebar-collapsable>.sidebar-collapsable-title>span>svg
+    this.children[1].children[0].classList.toggle("rotate-180");
+
+    // #sidebar .sidebar-collapsable>.sidebar-collapsable-items
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+
+  sb_collapsable.addEventListener("mouseover", function (e) {
+    if (ismobile) return;
+    // #sidebar .sidebar-collapsable>.sidebar-collapsable-title>span>svg
+    this.children[0].children[1].children[0].classList.add("rotate-180");
+
+    // #sidebar .sidebar-collapsable>.sidebar-collapsable-items
+    let content = this.children[1];
+    content.style.maxHeight = content.scrollHeight + "px";
+  });
+
+  sb_collapsable.addEventListener("mouseout", function (e) {
+    if (ismobile) return;
+    // #sidebar .sidebar-collapsable>.sidebar-collapsable-title>span>svg
+    this.children[0].children[1].children[0].classList.remove("rotate-180");
+
+    // #sidebar .sidebar-collapsable>.sidebar-collapsable-items
+    let content = this.children[1];
+    content.style.maxHeight = null;
+  });
+});
+
 //Event Listeners
 document.getElementById("menu").addEventListener("click", openSideBar);
 document.getElementById("menuh").addEventListener("click", openSideBar);
